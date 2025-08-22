1664f59504ac52cb0fb35470a35429565a212359
nebulemz <lemueljohnxd3@gmail.com>
2025-08-22 22:35:37 +0800

    UI initial commit

---
+++ ems-frontend/src/HelloWorld.tsx
@@ -0,0 +1,5 @@
+function HelloWorld() {
+    return <h1>Hellow World</h1>
+}
+
+export default HelloWorld;
--- ems-frontend/vite.config.ts
+++ ems-frontend/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    port:3000 //This is manually set-up in typing npm run dev -- --port 3000
+  }
 })
