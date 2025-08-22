08be7626d9f3ee4da3f3e7c1909f40e43f5fb929
nebulemz <lemueljohnxd3@gmail.com>
2025-08-23 00:19:09 +0800

    ts component practice

---
+++ ems-frontend/src/components/ListEmployeeController.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+type Props = {}
+
+const ListEmployeeController = (props: Props) => {
+  
+  
+  
+    return (
+    <div>ListEmployeeController</div>
+  )
+}
+
+export default ListEmployeeController
