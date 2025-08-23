78fe8e99d2b8d4e6df1dab71576b04806cc9d253
nebulemz <lemueljohnxd3@gmail.com>
2025-08-23 13:47:44 +0800

    employee router dom

--- ems-frontend/src/App.tsx
+++ ems-frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import ListEmployeeController from "./components/controller/ListEmployeeController.tsx";
 import HeaderComponent from "./components/layout/HeaderComponent.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HelloWorld from "./HelloWorld.tsx";
 
 function App() {
   return (
     <>
-      <HeaderComponent />
-      <ListEmployeeController />
+      <BrowserRouter>
+        <HeaderComponent />
+        <Routes>
+          //http://localhost:3000
+          <Route path='/' element = { <ListEmployeeController /> }></Route>
+          //http://localhost:3000/employees
+          <Route path='/employees' element = { <ListEmployeeController /> }></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ ems-frontend/src/components/layout/FooterComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const FooterComponent = (props: Props) => {
+  return (
+    <div>
+        <footer className='footer'>
+
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
